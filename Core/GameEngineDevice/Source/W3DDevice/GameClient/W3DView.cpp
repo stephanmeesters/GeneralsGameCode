643b3b5e745e3c1cbc822960cb37cc219487adb7
Mauller <26652186+Mauller@users.noreply.github.com>
2025-07-17 21:37:39 +0100

    fix(view): Adjust default camera height to compensate for screen aspect ratio

--- Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DView.cpp
+++ Core/GameEngineDevice/Source/W3DDevice/GameClient/W3DView.cpp
@@ -2234,10 +2234,25 @@ void W3DView::setPitchToDefault()
 //-------------------------------------------------------------------------------------------------
 void W3DView::setDefaultView(Real pitch, Real angle, Real maxHeight)
 {
+	// TheSuperHackers @fix Mauller 08/05/2026 Adjust the maximum camera height to compensate for screen aspect ratio
+	Real baseAspectRatio = (Real)DEFAULT_DISPLAY_WIDTH / (Real)DEFAULT_DISPLAY_HEIGHT;
+	Real currentAspectRatio = (Real)TheTacticalView->getWidth() / (Real)TheTacticalView->getHeight();
+	Real aspectRatioScale = 0.0f;
+
+	if (currentAspectRatio > baseAspectRatio)
+	{
+		aspectRatioScale = fabs((1 + (currentAspectRatio - baseAspectRatio)));
+	}
+	else
+	{
+		aspectRatioScale = fabs((1 - (baseAspectRatio - currentAspectRatio)));
+	}
+
 	// MDC - we no longer want to rotate maps (design made all of them right to begin with)
 	//	m_defaultAngle = angle * M_PI/180.0f;
 	setDefaultPitch(pitch);
-	m_maxHeightAboveGround = TheGlobalData->m_maxCameraHeight*maxHeight;
+	m_maxHeightAboveGround = TheGlobalData->m_maxCameraHeight * aspectRatioScale * maxHeight;
+	m_minHeightAboveGround = TheGlobalData->m_minCameraHeight * aspectRatioScale;
 	if (m_minHeightAboveGround > m_maxHeightAboveGround)
 		m_maxHeightAboveGround = m_minHeightAboveGround;
 }
@@ -2279,7 +2294,6 @@ void W3DView::setZoomToDefault()
 	m_heightAboveGround = m_maxHeightAboveGround;
 	m_zoom = getMaxZoom(m_pos.x, m_pos.y);
 
-	stopDoingScriptedCamera();
 	m_CameraArrivedAtWaypointOnPathFlag = false;
 	m_cameraAreaConstraintsValid = false;
 	m_recalcCamera = true;
