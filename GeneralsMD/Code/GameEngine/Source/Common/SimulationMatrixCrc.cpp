f65c337362e47fdde61d66fee7192623dfec48a2
stm <14291421+stephanmeesters@users.noreply.github.com>
2026-01-10 14:00:45 +0100

    bb

--- GeneralsMD/Code/GameEngine/Source/Common/SimulationMatrixCrc.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/SimulationMatrixCrc.cpp
@@ -1,6 +1,6 @@
 /*
 **	Command & Conquer Generals Zero Hour(tm)
-**	Copyright 2025 TheSuperHackers
+**	Copyright 2026 TheSuperHackers
 **
 **	This program is free software: you can redistribute it and/or modify
 **	it under the terms of the GNU General Public License as published by
